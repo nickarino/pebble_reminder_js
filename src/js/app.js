aaf2a1d4e6161f81fd77e20318936fbd5021a8c2
Nick Skriloff <skriloffn@darden.virginia.edu>
2015-04-26 18:00:17 -0400

    all JSLint errors fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,58 +1,131 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
+(function () {
+    'use strict';
+    /*global console, require, showBodyMessage*/
+    var UI = require('ui'),
+        ajax = require('ajax'),
+        Vector2 = require('vector2'),
+        Accel = require('ui/accel'),
+        Vibe = require('ui/vibe'),
 
-var UI = require('ui');
-var Vector2 = require('vector2');
+        // require firebase.js
+        //var Firebase = require('/src/js/firebase.js');
+        //var Firebase = require('firebase');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
-});
+        //firebase ref
+        //var firebaseRef = new Firebase("https://pebblereminder.firebaseio.com/");
+        //var chatRef = new Firebase('https://fiery-fire-2493.firebaseio.com/chat');
 
-main.show();
+        //set up cards
+        prompt = new UI.Card({
+            title: 'Posture',
+            //icon: 'images/menu_icon.png',
+            //subtitle: '',
+            body: 'Did you have proper posture for the last hour? '
+        }),
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
+        answer = new UI.Card({
+            //will be set later
+        }),
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
+        //from https://github.com/pebble-hacks/hackathon-watchface/blob/master/src/app.js
+        watchFace = new UI.Window({
+            fullscreen: true
+        }),
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+        timeText = new UI.TimeText({
+            position: new Vector2(0, 144),
+            size: new Vector2(144, 24),
+            font: 'gothic-18-bold',
+            text: '%H:%M:%S',
+            textAlign: 'center'
+        }),
+
+        hackathonLogo = new UI.Image({
+            position: new Vector2(0, 0),
+            size: new Vector2(144, 144),
+            backgroundColor: 'clear',
+            image: 'images/pebble.png'
+        }),
+        //var cityName = 'London';
+        //var URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+        URL = 'https://pebblereminder.firebaseio.com/message_list.json';
+
+    answer.hide(); //for good measure
+
+    watchFace.add(hackathonLogo);
+    watchFace.add(timeText);
+    watchFace.show();
+    //end watchface
+
+    function callAjax() {
+        ajax({
+            url: 'http://api.theysaidso.com/qod.json',
+            type: 'json'
+        },
+            function (data, status, request) {
+                console.log('Quote of the day is: ' + data.contents.quote);
+            },
+            function (error, status, request) {
+                console.log('The ajax request failed: ' + error);
+            }
+            );
+    }
+
+    prompt.action({
+        up: 'images/yes.png',
+        down: 'images/no.png'
+    });
+
+    prompt.on('click', 'up', function () {
+        //send request to firebase
+        //set interval
+        showBodyMessage('Good Job');
+        console.log('Hi Nick!!!');
+        callAjax();
+    });
+
+    prompt.on('click', 'down', function () {
+        //send request to firebase
+        showBodyMessage(' Its Ok. Pick back up the next hour');
+    });
+
+    ajax({
+        url: URL,
+        type: 'json'
+    },
+        function (data) {
+            // Success!
+            console.log('Successfully fetched weather data!');
+            // Extract data
+            //var location = data.name;
+            //var temperature = Math.round(data.main.temp - 273.15) + 'C';
+            //console.log('location is' + location + 'and temp is ' + temperature);
+            //console.log('location is' + location);
+            // Always upper-case first letter of description
+            console.log('Nick the data is' + JSON.stringify(data));
+            //   var description = data.weather[0].description;
+            //   description = description.charAt(0).toUpperCase() + description.substring(1);
+            //     console.log('description is ' + description);
+        },
+        function (error) {
+            // Failure!
+            console.log('Failed fetching weather data: ' + error);
+        }
+        );
+
+    function showBodyMessage(answerBody) {
+        answer.body(answerBody);
+        prompt.hide();
+        answer.show();
+        setTimeout(function () {
+            watchFace.show(); //end of the state
+            //firebaseRef.set({id:1, description:'test', location:'robotsconf'});
+        }, 2000);
+    }
+
+    prompt.show();
+
+    // Send a long vibration to the user wrist
+    Vibe.vibrate('long');
+
+}());
